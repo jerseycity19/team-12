fbeb09b7ca956f2a434c7ac38d1826f6e8e8ab41
bsoong <bsoong99@gmail.com>
2019-09-27 20:47:59 -0400

    First Branch push, Backend setup

---
+++ backend/data/index.js
@@ -0,0 +1,9 @@
+const accountData = require("./account");
+const forumData = require("./forum");
+const toDoData = require("./toDo")
+
+module.exports = {
+  users: accountData,
+  forum: forumData,
+  toDoData: toDoData
+}
