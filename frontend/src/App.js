e8bf1635395b7329d5100c2b5be0ec8d40304a01
Solepride <leman.yan80@myhunter.cuny.edu>
2019-09-28 02:47:22 -0400

    Need to add todos and achievement sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Homepage from './Components/Homepage/Homepage';
 import Navbar from "./Components/Navbar/Navbar";
 import Trackpage from './Components/Trackpage/Trackpage'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 import './App.css';
 
--- frontend/src/Components/Trackpage/Trackpage.js
+++ frontend/src/Components/Trackpage/Trackpage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import './Trackpage.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class Trackpage extends Component {
     render(){
     	return (
 
-	<div>
+	<div className="body">
 		<div className ="title">
 			<p>TAKE ACTION</p>
 			<h1>Raise Awareness Track</h1>
@@ -15,6 +17,17 @@ class Trackpage extends Component {
 				<p>Together we can reduce the stigma and secrecy associated with addiction. Earn achievements and xp and make a difference at the same time!</p>
 			</div>
 		</div>
+
+		<div className="wrapper">
+		  <div className="column-one">
+		  	<h2>To dos</h2>
+		  </div>
+		  <div className="column-two">
+		  	<h2>Achievements</h2>
+	  	  </div>
+		</div>
+
+
 	</div>
     	)
     }
