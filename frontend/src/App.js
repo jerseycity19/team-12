d990031e02c050ba99deb997166048bd756dd3b3
trevornogues <noguestj@gmail.com>
2019-09-28 04:17:54 -0400

    need to shift stuff down

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Homepage from './Components/Homepage/Homepage';
 import Navbar from "./Components/Navbar/Navbar";
-<<<<<<< HEAD
-import Forum from "./Components/Forum/Forum";
+import Trackpage from "./Components/Trackpage/Trackpage";
 import Login from "./Components/Login/Login";
-=======
-import Trackpage from './Components/Trackpage/Trackpage'
->>>>>>> 01b164a578fd9d0d14f7caaf52da97655a4d6ba3
+import Survey from "./Components/Survey/Survey";
+
 
 import './App.css';
 
@@ -17,13 +15,9 @@ function App() {
       <Navbar/>
       <Router>
           <Route exact path="/" component={Homepage} />
-<<<<<<< HEAD
-          <Route exact path="/Forum" component={Forum} />
-          <Route exact path="/Login" component={Login} />
-=======
           <Route exact path="/track" component={Trackpage} />
-
->>>>>>> 01b164a578fd9d0d14f7caaf52da97655a4d6ba3
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/Survey" component={Survey} />
         </Router>
           
     </div>
