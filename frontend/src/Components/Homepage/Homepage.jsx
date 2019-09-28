e9a61b73de2f32226336ff7ac91ddca14410f8ed
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2019-09-27 21:29:29 -0400

    pushing homepage component

---
+++ frontend/src/Components/Homepage/Homepage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {Navbar} from 'react-bootstrap';
+import './Homepage.css'
+class Homepage extends Component {
+  render(){
+    return (
+      <div>
+        <Navbar>skdfslkj</Navbar>
+        hello
+      </div>
+    )
+  }
+}
+
+export default Homepage;
