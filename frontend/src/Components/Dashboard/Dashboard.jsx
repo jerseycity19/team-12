1cd67ed5a4339b9fa6284058feda9be968dc713c
Brandon Soong <bsoong99@gmail.com>
2019-09-28 09:25:17 -0400

    Dashboard

--- frontend/src/Components/Dashboard/Dashboard.jsx
+++ frontend/src/Components/Dashboard/Dashboard.jsx
@@ -1,20 +1,25 @@
 import React, { Component } from 'react'
-import {Button} from 'react-bootstrap'
+import {Card, Button} from 'react-bootstrap'
 import './Dashboard.scss';
 
 class Dashboard extends Component {
   render(){
     return (
       <div className = "outside">
-      <h1>Events near you!</h1>
-      <div class="Calendar">
-        <h3>Celebrate Recovery</h3>
-        <p class="w3-opacity">New York, New York</p>
-        <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
-        <p><button class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> RSVP</button></p>
-      </div>
-      </div>
+      <img src={require('./picture.png')}></img>
 
+      <img src={require('./bronze-league.png')} className = "bronze"></img>
+      <div class="w3-card">
+        <img src={require("./event.png")} alt="John" className = "event-image"/>
+        <div class="w3-container">
+          <h3>Comissoner Talk</h3>
+          <p class="w3-opacity">New York City</p>
+          <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+          <p><button class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> RSVP</button></p>
+          <p><button class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Volunteer</button></p>
+        </div>
+        </div>
+        </div>
       )
     }
   }
