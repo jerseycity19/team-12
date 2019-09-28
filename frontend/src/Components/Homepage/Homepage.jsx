50219b6e7ad0b9c0bdbcee20c1a637080338e543
trevornogues <noguestj@gmail.com>
2019-09-28 03:01:27 -0400

    edited homepage text

--- frontend/src/Components/Homepage/Homepage.jsx
+++ frontend/src/Components/Homepage/Homepage.jsx
@@ -18,23 +18,24 @@ class Homepage extends Component {
         <div class="w3-row-padding w3-center" >
           <div class="w3-quarter">
             <i class="fa fa-desktop w3-margin-bottom w3-jumbo w3-center"></i>
-            <p class="w3-large">Responsive</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+            <p class="w3-large">Communicate</p>
+            <p>Interact with other users on our site. You are not alone and many others are sharing their stories <i>here.</i></p>
           </div>
           <div class="w3-quarter">
             <i class="fa fa-heart w3-margin-bottom w3-jumbo"></i>
-            <p class="w3-large">Passion</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+            <p class="w3-large">End the Stigma</p>
+            <p>More people than you might expect are impacted by addiction. Click <i>here</i> to see the stats.</p>
           </div>
           <div class="w3-quarter">
             <i class="fa fa-diamond w3-margin-bottom w3-jumbo"></i>
             <p class="w3-large">Design</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+            <p>Donote</p>
+            <p>Click <i>here</i> to donate. 100% of donations goes to support programs to help end addiction.</p>
           </div>
           <div class="w3-quarter">
             <i class="fa fa-cog w3-margin-bottom w3-jumbo"></i>
-            <p class="w3-large">Support</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+            <p class="w3-large">Need Help?</p>
+            <p>Click <i>here</i> to learn more about treatment options near you.</p>
           </div>
           </div>
         </div>
