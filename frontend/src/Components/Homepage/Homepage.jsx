7c25dcec93a2d1abf385734f0976da2d63c893c1
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2019-09-28 01:42:51 -0400

    adding user stories

--- frontend/src/Components/Homepage/Homepage.jsx
+++ frontend/src/Components/Homepage/Homepage.jsx
@@ -48,6 +48,49 @@ class Homepage extends Component {
         </div>
         <div className="event-header">
           <h2>Upcoming Events</h2>
+          <div  className="event-grid">
+            <div class="w3-card">
+              <img src="/w3images/team2.jpg" alt="John"/>
+              <div class="w3-container">
+                <h3>John Doe</h3>
+                <p class="w3-opacity">CEO & Founder</p>
+                <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+                <p><button class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</button></p>
+              </div>
+            </div>
+            <div class="w3-card">
+              <img src="/w3images/team2.jpg" alt="John"/>
+              <div class="w3-container">
+                <h3>John Doe</h3>
+                <p class="w3-opacity">CEO & Founder</p>
+                <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+                <p><button class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</button></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="user-stories">
+          <h2>Upcoming Events</h2>
+          <div  className="event-grid">
+            <div class="w3-card">
+              <img src="/w3images/team2.jpg" alt="John"/>
+              <div class="w3-container">
+                <h3>John Doe</h3>
+                <p class="w3-opacity">CEO & Founder</p>
+                <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+                <p><button class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</button></p>
+              </div>
+            </div>
+            <div class="w3-card">
+              <img src="/w3images/team2.jpg" alt="John"/>
+              <div class="w3-container">
+                <h3>John Doe</h3>
+                <p class="w3-opacity">CEO & Founder</p>
+                <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+                <p><button class="w3-button w3-light-grey w3-block"><i class="fa fa-envelope"></i> Contact</button></p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       
