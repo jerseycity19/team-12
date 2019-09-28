f03a271e38510579eadad66d04b42c9d31fa2180
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2019-09-28 01:03:53 -0400

    adding forum box

--- frontend/src/Components/Homepage/Homepage.jsx
+++ frontend/src/Components/Homepage/Homepage.jsx
@@ -18,25 +18,32 @@ class Homepage extends Component {
         <div class="w3-row-padding w3-center" >
           <div class="w3-quarter">
             <i class="fa fa-desktop w3-margin-bottom w3-jumbo w3-center"></i>
-            <p class="w3-large">Responsive</p>
+            <p class="w3-large">Get informed</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
           </div>
           <div class="w3-quarter">
             <i class="fa fa-heart w3-margin-bottom w3-jumbo"></i>
-            <p class="w3-large">Passion</p>
+            <p class="w3-large">End the Stigma</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
           </div>
           <div class="w3-quarter">
             <i class="fa fa-diamond w3-margin-bottom w3-jumbo"></i>
-            <p class="w3-large">Design</p>
+            <p class="w3-large">Advocate for Change</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
           </div>
           <div class="w3-quarter">
             <i class="fa fa-cog w3-margin-bottom w3-jumbo"></i>
-            <p class="w3-large">Support</p>
+            <p class="w3-large">Join the community</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
           </div>
           </div>
+        </div>
+        <div className = "gray-header">
+          <div className ="text-box">
+            <h1>Forum for Open Discussion</h1>
+            <p>This is a safe space for our account members to and vistors to engage in open discussion. Please feel free to make a post sharing an experience, question or thought with our community</p>
+          </div>
+          
         </div>
       </div>
       
