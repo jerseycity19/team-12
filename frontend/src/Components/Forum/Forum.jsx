48294e37aa333b53afe02c9953418d847d8306d5
trevornogues <noguestj@gmail.com>
2019-09-28 01:12:08 -0400

    forum pic

--- frontend/src/Components/Forum/Forum.jsx
+++ frontend/src/Components/Forum/Forum.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import './Forum.css';
+import './Forum.scss';
 
 class Forum extends Component {
   render(){
     return (
-      <div className = "top-header">
+      <div className = "forum-header">
         
         <h1>Forum for Open Discussions</h1>
         <p>The is a safe space for our account members and visitors to engage in open discussion. 
